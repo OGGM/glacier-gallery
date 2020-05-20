c2b5d439846dca4cc9bfe89beaaeaa57d35b9bfb
Zora Schirmeister <zora.schirmeister@student.uibk.ac.at>
2020-05-20 07:28:13 +0200

    add option for translation, add two import files for texts, links and glaciers

---
+++ international.py
@@ -0,0 +1,413 @@
+# -*- coding: utf-8 -*-
+supported_languages = ['en', 'de']
+
+trads = {
+    'Title_hp':
+        {
+            'en': '<div style="font-size:38px; color: #326a86; font-weight: bold" >Glacier Gallery</div>',
+            'de': '<div style="font-size:38px; color: #326a86; font-weight: bold" >Gletschergallerie</div>'
+        },  
+    
+    'instruction':
+        {
+            'en': '<div style="font-size:15px; color: #326a86" >Explore glaciers of the world by moving the mouse over the colored dots on the map. <br> When you <b>click</b> on them, information about the glacier type will appear in the box on the right.</div>',
+            'de': '<div style="font-size:15px; color: #326a86" >Entdecke verschiedene Gletscher dieser Erde. Fahre dazu mit der Maus über die farbigen Markierungen auf der Karte. <br>Wenn du sie <b>anklickst</b> erscheinen Informationen zum entsprechenden Gletschertyp in der Box auf der rechten Seite.</div>'
+        }, 
+    
+    'link_to_license_intro':
+        {
+            'en': '<div>Links to licenses of the images:</div>',
+            'de': '<div> Links zu den Bilderlizenzen:</div>'
+        },  
+    
+    'links_pic_src_intro':
+        {
+            'en': '<div>Links to sources of the images:</div>',
+            'de': '<div> Links zu den Bilderquellen:</div>'
+        },  
+    
+    'tab_title_1':
+        {
+            'en': 'Glacier type',
+            'de': 'Gletschertyp'
+        },
+    
+    'tab_title_2':
+        {
+            'en': 'Glacier definition',
+            'de': 'Gletscherdefinition'
+        },
+    
+    
+    'Glacier_title':
+        {
+            'en': "Glaciers",
+            'de': "Gletscher"
+        },
+    
+    'type_pure_ice':
+        {
+            'en': "Pure ice glacier",
+            'de': "Reiner Eisgletscher"
+        },
+    
+    'type_calving':
+        {
+            'en': "Calving glacier",
+            'de': "Kalbender Gletscher"
+        },
+    
+    'type_debris_covered':
+        {
+            'en': "Debris covered glacier",
+            'de': "Blockgletscher"
+        },
+    
+    'type_ice_cap':
+        {
+            'en': "Ice cap",
+            'de': "Eiskappe"
+        },
+    
+    # Textbreite wichtig
+    'Glacier_definition':
+        {
+            'en': """
+    Glaciers are defined as a perennial mass of 
+    land ice that forms through the compression 
+    of snow. The ice mass has a past or 
+    present flow, which is caused by internal 
+    deformation or sliding at the base. Glaciers 
+    accumulate snow in high altitudes and loose 
+    mass by ablation at low altitudes and/or 
+    discharge into the sea or lakes.
+    
+    Furthermore, glaciers are separated into 
+    unconstrained and constrained glaciers. 
+    Unconstrained glaciers include ice sheets, 
+    ice caps and ice streams. Constrained 
+    glaciers include ice fields, valley glaciers, 
+    transection glaciers (interconnected valley 
+    glaciers), piedmont glaciers, cirque and 
+    niche glaciers.
+    """,
+            'de': """
+    Gletscher sind langjährig bestehende 
+    Landeismassen, die durch die Kompression 
+    von Schnee entstehen und erhalten bleiben.
+    Sie besitzen die Eigenschaft zu fließen, 
+    dieser Vorgang kann auch in der
+    Vergangenheit stattgefunden haben. Durch 
+    interne Verformungen oder durch Gleiten 
+    auf dem Gletscherbett bewegen sich Gletscher 
+    abwärts. Im oberen Teil von Gletschern häuft 
+    sich Schnee an (Akkumulation). 
+    Im unteren Teil verlieren sie an Masse durch 
+    Schmelzprozesse und z. B. Kalben, also Abbruch 
+    von großen Eisstücken.
+    Gletscher sind in begrenzte und unbegrenzte 
+    Typen eingeteilt. Unbegrenzte Gletscher sind 
+    z.B. Eisschilde, Eiskappen oder Eisströme.
+    Begrenzte Gletscher sind Eisfelder, 
+    Talgletscher, "Transektionsgletscher" 
+    (verbundene Talgletscher), Piedmontgletscher, 
+    Kargletscher und Nischengletscher. 
+    """            
+        },
+    
+    'Glacier_text_source':
+        {
+            'en':"""
+    see IPCC, 2013: Climate Change 2013: The Physical Science Basis.
+    Contribution of Working Group I to the Fifth Assessment Report of 
+    the Intergovernmental Panel on Climate Change [Stocker, T.F., D. 
+    Qin, G.-K. Plattner, M. Tignor, S.K. Allen, J. Boschung, A. Nauels, Y. 
+    Xia, V. Bex and P.M. Midgley (eds.)]. Cambridge University Press, 
+    Cambridge, United Kingdom and New York, NY, USA, 1535 pp.,
+    http://www.antarcticglaciers.org/glacier-processes/glacier-types/
+    types-of-glaciers/
+    """,
+            'de':"""
+    siehe IPCC, 2013: Climate Change 2013: The Physical Science Basis. 
+    Contribution of Working Group I to the Fifth Assessment Report of 
+    the Intergovernmental Panel on Climate Change [Stocker, T.F., D. 
+    Qin, G.-K. Plattner, M. Tignor, S.K. Allen, J. Boschung, A. Nauels, Y. 
+    Xia, V. Bex and P.M. Midgley (eds.)]. Cambridge University Press, 
+    Cambridge, United Kingdom and New York, NY, USA, 1535 pp.,
+    http://www.antarcticglaciers.org/glacier-processes/glacier-types/
+    types-of-glaciers/
+    """
+        },    
+    
+    'pure_ice_text':
+        {
+            'en':"""
+    Most glaciers are pure ice glaciers. That 
+    means that they are not covered by debris 
+    nor riddled with rocks. They form all 
+    different shapes that are described in the 
+    glacier definition. You can find this kind of
+    glacier on all continents.
+    
+    
+    Please note that we put all glaciers in this
+    category that do not fit to the other glacier 
+    types listed, but exhibit the pure ice glacier 
+    characteristic. 
+    """,
+            'de': """
+    Diese Gletscher bestehen aus Eis ohne 
+    gröbere Verunreinigungen. Das bedeutet, dass 
+    sie weder mit einer Schicht aus Schutt bedeckt, 
+    noch mit Steinen durchsetzt sind. Sie 
+    können alle Formen annehmen, die in der 
+    Gletscherdefinition beschrieben sind und 
+    kommen auf allen Kontinenten vor.
+    
+    Achtung: In der Karte sind dieser Kategorie 
+    Gletscher zugeordnet, die den hier genannten 
+    Charakteristika entsprechen, und dabei nicht in 
+    die anderen Kategorien passen.  
+    """
+    },
+    
+    'pure_ice_source':
+        {'en': """
+    see IPCC, 2013: Climate Change 2013: The Physical Science Basis.
+    Contribution of Working Group I to the Fifth Assessment Report of 
+    the Intergovernmental Panel on Climate Change [Stocker, T.F., D. 
+    Qin, G.-K. Plattner, M. Tignor, S.K. Allen, J. Boschung, A. Nauels, Y. 
+    Xia, V. Bex and P.M. Midgley (eds.)]. Cambridge University Press, 
+    Cambridge, United Kingdom and New York, NY, USA, 1535 pp.,
+    http://www.antarcticglaciers.org/glacier-processes/glacier-types/
+    types-of-glaciers/
+    """,
+         'de':"""
+    siehe IPCC, 2013: Climate Change 2013: The Physical Science Basis. 
+    Contribution of Working Group I to the Fifth Assessment Report of 
+    the Intergovernmental Panel on Climate Change [Stocker, T.F., D. 
+    Qin, G.-K. Plattner, M. Tignor, S.K. Allen, J. Boschung, A. Nauels, Y. 
+    Xia, V. Bex and P.M. Midgley (eds.)]. Cambridge University Press, 
+    Cambridge, United Kingdom and New York, NY, USA, 1535 pp.,
+    http://www.antarcticglaciers.org/glacier-processes/glacier-types/
+    types-of-glaciers/
+    """
+        },
+    
+    'calving_text':
+        {
+            'en':'''
+    A glacier calves when a huge mass of ice 
+    breaks off at the front of the glacier. 
+    Most of these glaciers are 
+    water-terminating, that means they end
+    in lakes, fjords or the sea. Either they 
+    float on the water surface or they are 
+    grounded. 
+    Calving glaciers exhibit many crevasses 
+    due to their relatively fast flow and
+    an extensional (stretching) flow at 
+    their lower end. Another feature is their
+    near-vertical ice cliff where calving
+    happens. Depending on the glacier 
+    thickness the terminus is between ten to
+    eighty meters thick. How often and how
+    much a glacier calves is described by the
+    calving rate, which depends on the 
+    variations in the longitudinal strain 
+    rates and the ice velocities, among others. 
+    ''',
+            'de': '''
+    Das Abbrechen großer Eisstücke am unteren 
+    Ende eines Gletscher wird als Kalben 
+    bezeichnet. Viele kalbende Gletscher enden 
+    im Meer, in einem Fjord oder einem See. Sie 
+    reichen dort bis zum Grund oder schwimmen an 
+    der Oberfläche. 
+    Kalbende Gletscher fließen relativ schnell 
+    und werden an ihrem unteren Ende stark 
+    gedehnt. Daher bilden sich viele 
+    Gletscherspalten. An der Eisfront bildet sich 
+    durch den Abbruch ein fast-vertikales Eiskliff, 
+    das 10 bis 80 m hoch sein kann.
+    Wie oft und wieviel ein Gletscher kalbt ist z.B. 
+    abhängig von der Deformationsgeschwindigkeit 
+    und der Fließgeschwindigkeit des Eises. 
+    '''
+    },
+    
+    'calving_glacier_source':
+        {
+            'en': """
+    see Bishop, M. P., Björnsson, H., Haeberli, W., Oerlemans, J., 
+    Shroder, J. F., & Tranter, M. (2011). Encyclopedia of snow, ice 
+    and glaciers. Springer Science & Business Media.
+    """,
+            'de': """
+    siehe Bishop, M. P., Björnsson, H., Haeberli, W., Oerlemans, J., 
+    Shroder, J. F., & Tranter, M. (2011). Encyclopedia of snow, ice 
+    and glaciers. Springer Science & Business Media.
+    """        
+    },
+    
+    'debris_text':
+        {
+            'en':'''
+    A layer of debris covers the lower part of 
+    the glacier’s surface. This type of 
+    glacier can be found in all major mountain 
+    regions of the world, e.g. in the Himalayan 
+    it is the predominant form of glaciation.
+    The layer of debris forms by rockfall from 
+    nearby mountain faces with high 
+    denudation rates, rock avalanching and 
+    mixed snow/ice/rock avalanching. The 
+    resulting distribution and type of the debris 
+    layer influences the glacier’s dynamics, 
+    rheology and mass balance, and especially 
+    its ablation. Depending on the thickness of 
+    the layer, melting increases or decreases: 
+    Dispersed debris cover and layers thinner 
+    than ~15-80 mm enhance melting, because 
+    the rocks heat up under solar radiation. At 
+    the contrary thicker layers have an 
+    insulating effect and protect the ice from 
+    insolation.
+    ''',
+            'de': '''
+    Der untere Teil dieser Gletscher ist von einer 
+    Schuttschicht bedeckt. Diese entsteht durch 
+    Felsstürze von angrenzenden Felswänden, 
+    Steinlawinen und gemischten Schnee-Eis-Stein-
+    Lawinen.
+    Die Menge und Verteilung des Schutts auf der 
+    Gletscheroberfläche haben Einfluss auf die 
+    Gletscherdynamik, auf sein Fließverhalten und
+    seine Massenbilanz (vor allem auf die 
+    Schmelzrate). Die Dicke der Schicht bestimmt, 
+    ob Schmelzen verstärkt oder abgeschwächt 
+    wird: Liegt der Schutt verstreut oder ist die 
+    Schicht dünner als ~15-80 mm wird das 
+    Schmelzen verstärkt. Der Grund ist, dass die 
+    Steine durch die Sonne erhitzt werden. Dagegen 
+    wirkt eine dickere Schicht isolierend und 
+    schützt das Eis vor Sonneneinstrahlung.
+    Dieser Gletschertyp existiert in allen größeren 
+    Bergregionen der Welt, im Himalaya ist er 
+    die vorherrschende Gletscherform.
+    '''
+    },
+    
+    'debris_glacier_source':
+        {
+            'en': """
+    see Bishop, M. P., Björnsson, H., Haeberli, W., Oerlemans, J., 
+    Shroder, J. F., & Tranter, M. (2011). Encyclopedia of snow, ice 
+    and glaciers. Springer Science & Business Media.
+    """,
+            'de': """
+    siehe Bishop, M. P., Björnsson, H., Haeberli, W., Oerlemans, J., 
+    Shroder, J. F., & Tranter, M. (2011). Encyclopedia of snow, ice 
+    and glaciers. Springer Science & Business Media.
+    """        
+    },
+    
+    'ice_cap_text':
+        {
+            'en':'''
+    Ice caps are defined as huge ice masses 
+    that cover land on a large scale and 
+    do not extend 50000 km² (approximately 
+    the area of Costa Rica). This definition
+    excludes Greenland and Antarctic ice 
+    sheets, which are much larger.
+    Ice caps can be found in polar or 
+    subtemperate regions, where 
+    accumulation of snow commonly exceeds 
+    ablation. The formation process is 
+    described in the following: glaciers 
+    grow in the higher parts of the mountains, 
+    nearby glaciers merge and build ice bands 
+    around the mountains. These ice bands 
+    fuse with the ones from other mountains. 
+    As a result they build the ice caps 
+    that cover large parts of the land. 
+    Ice caps submerge the topography beneath 
+    while only some peaks stick out of the 
+    ice, which are called “nunataks”. 
+    ''',
+            'de': '''
+    Eiskappen sind riesige Eismassen, die 
+    Landmasse bedecken und dabei maximal 
+    50000 km² groß sind (circa die Fläche 
+    Costa Ricas). Damit sind der Grönländische
+    und Antarktische Eisschild ausgeschlossen, 
+    da sie viel größer sind. 
+    Eiskappen existieren in Regionen, in denen 
+    es so kalt ist, dass weniger Schnee schmilzt 
+    als angesammelt wird. Sie bilden sich auf 
+    folgendem Weg: 
+    In höher gelegenen Gebieten in den Bergen 
+    bilden sich Gletscher. Nebeneinander liegende 
+    Gletscher eines Berges fließen ineinander und 
+    verbinden sich. Zusammen bilden sie ein 
+    Eisband um den Berg, das sich wiederum mit 
+    einem benachbarten Eisband verbindet. So 
+    entstehen Eiskappen, die große Flächen von 
+    Land bedecken. Sie "überschwemmen" quasi die 
+    Oberfläche, wobei einzelne Gipfel 
+    herausschauen können, die "Nunataks" genannt 
+    werden.
+    '''
+    },
+    
+    'ice_cap_source':
+        {
+            'en': """
+    see Bishop, M. P., Björnsson, H., Haeberli, W., Oerlemans, J., 
+    Shroder, J. F., & Tranter, M. (2011). Encyclopedia of snow, ice 
+    and glaciers. Springer Science & Business Media.
+    """,
+            'de': """
+    siehe Bishop, M. P., Björnsson, H., Haeberli, W., Oerlemans, J., 
+    Shroder, J. F., & Tranter, M. (2011). Encyclopedia of snow, ice 
+    and glaciers. Springer Science & Business Media.
+    """        
+    },
+    
+    'TerminusType_land':
+        {
+            'en': 'Land-terminating',
+            'de': 'Endet an Land'
+        },
+    
+    'TerminusType_marine':
+        {
+            'en': 'Marine-terminating',
+            'de': 'Endet im Meer'
+        }    
+}
+
+links = {
+    'links_license': """<div> 
+                            <a href="https://creativecommons.org/licenses/by-sa/3.0/igo/deed.en">CC BY-SA 3.0 IGO </a> <br>
+                            <a href="https://creativecommons.org/licenses/by-nc/4.0/"> CC BY-NC</a> <br>
+                            <a href="https://creativecommons.org/licenses/by-sa/3.0/">CC BY-SA 3.0 </a> <br>
+                            <a href="https://creativecommons.org/licenses/by/3.0/deed.en">CC BY 3.0 (Creative Commons Attribution 3.0 Unported)</a>
+                      </div>""",
+    'links_pics': """<div>
+                        <a href="http://nsidc.org/data/glacier_photo/search/image_info/lemoncreek20070917" >Lemon Creek Glacier</a> <br>
+                        <a href="https://commons.wikimedia.org/wiki/File:Columbia_Glacier_(Alaska)_by_Sentinel-2.jpg" >Columbia Glacier</a> <br>
+                        <a href="http://nsidc.org/data/glacier_photo/search/image_info/white2012059902?order=true" >White Glacier</a> <br>
+                        <a href="http://nsidc.org/data/glacier_photo/search/image_info/mittivakkat20130827?order=true" > Mittivakkat Glacier</a> <br>
+                        <a href="http://nsidc.org/data/glacier_photo/search/image_info/briksdalsbreen20090730?order=true" >Briksdalsbreen</a> <br>
+                        <a href="http://nsidc.org/data/glacier_photo/search/image_info/hintereisferner20060912?order=true" >Hintereisferner</a> <br>
+                        <a href="https://commons.wikimedia.org/wiki/File:Grosser_Aletschgletscher_3196.JPG" >Großer Aletschgletscher</a> <br>
+                        <a href="http://nsidc.org/data/glacier_photo/search/image_info/tbilisa20109999" >Tbilisa Glacier</a> <br>
+                        <a href="http://nsidc.org/data/glacier_photo/search/image_info/lewis20120223" >Lewis Glacier</a> <br>
+                        <a href="https://commons.wikimedia.org/wiki/File:Artesonraju_Peru.jpg" >Artesonraju</a> <br>
+                        <a href="https://eol.jsc.nasa.gov/SearchPhotos/photo.pl?mission=ISS016&roll=E&frame=24897" >Perito Moreno</a> <br>
+                        <a href="https://commons.wikimedia.org/wiki/File:Lower_Tasman_Glacier_towards_Minarets.jpg" >Tasman Glacier</a> <br>
+                        <a href="http://nsidc.org/data/glacier_photo/search/image_info/bahiadeldiablo20040224" >Bahia Del Diablo Glacier</a>
+                   </div>"""
+}
