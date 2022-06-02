8772c1927182174e6ae715b7a5753c589dc82b80
Fabien Maussion <fabien.maussion@uibk.ac.at>
2022-06-02 15:59:50 +0200

    New binder

--- binder/panelserverextension.py
+++
@@ -1,6 +0,0 @@
-from subprocess import Popen
-
-def load_jupyter_server_extension(nbapp):
-    """serve the .ipynb directory with bokeh server"""
-    Popen(["panel", "serve", "explorer.ipynb", "--allow-websocket-origin=*"])
-
